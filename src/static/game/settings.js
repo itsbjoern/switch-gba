8fa2394149090934b7a1f62da2e450f66202c77b
Björn Friedrichs <bjoern@friedrichs1.de>
2019-07-15 19:08:27 +0100

    Fix endless loop

--- src/static/game/settings.js
+++ src/static/game/settings.js
@@ -125,7 +125,7 @@ class Settings {
       var currentNode = settingNode.getElementsByClassName("current")[0];
       if (currentNode.innerHTML !== "" + setting.currentValue) {
         var wrapNode = settingNode.getElementsByClassName("wrap-container")[0];
-        for (var j = 0; i < wrapNode.childNodes.length; j++) {
+        for (var j = 0; j < wrapNode.childNodes.length; j++) {
           var iterNode = wrapNode.childNodes[i];
           var dist = i - setting.index;
           var childType = dist === 0 ? "current" : dist < 0 ? "prev" : "next";
